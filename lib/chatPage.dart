ddb603f1fe25121da3fb98a601026a3c9726d199
Takatoshi-ish <ishiguro.taka@gmail.com>
2022-08-14 00:11:54 +0900

    編集ボタンを追加しよう

--- lib/chatPage.dart
+++ lib/chatPage.dart
@@ -54,87 +54,94 @@ class _ChatPageState extends State<ChatPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('チャット'),
-        // actions プロパティにWidgetを与えると右端に表示されます。
-        actions: [
-          // tap 可能にするために InkWell を使います。
-          InkWell(
-            onTap: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) {
-                    return const MyPage();
-                  },
-                ),
-              );
-            },
-            child: CircleAvatar(
-              backgroundImage: NetworkImage(
-                FirebaseAuth.instance.currentUser!.photoURL!,
-              ),
-            ),
-          )
-        ],
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: StreamBuilder<QuerySnapshot<Post>>(
-              // stream プロパティに snapshots() を与えると、コレクションの中のドキュメントをリアルタイムで監視することができます。
-              stream: postsReference.orderBy('createdAt').snapshots(),
-              // ここで受け取っている snapshot に stream で流れてきたデータ入っています。
-              builder: (context, snapshot) {
-                // docs には Collection に保存されたすべてのドキュメントが入ります。
-                // 取得までには時間がかかるのではじめは null が入っています。
-                // null の場合は空配列が代入されるようにしています。
-                final docs = snapshot.data?.docs ?? [];
-                return ListView.builder(
-                  itemCount: docs.length,
-                  itemBuilder: (context, index) {
-                    // data() に Post インスタンスが入っています。
-                    // これは withConverter を使ったことにより得られる恩恵です。
-                    // 何もしなければこのデータ型は Map になります。
-                    final post = docs[index].data();
-                    return PostWidget(post: post);
-                  },
+    // Scaffold 全体を GestureDetector で囲むことでタップ可能になります。
+    return GestureDetector(
+      onTap: () {
+        // キーボードを閉じたい時はこれを呼びます。
+        primaryFocus?.unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('チャット'),
+          // actions プロパティにWidgetを与えると右端に表示されます。
+          actions: [
+            // tap 可能にするために InkWell を使います。
+            InkWell(
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return const MyPage();
+                    },
+                  ),
                 );
               },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: TextFormField(
-              // 上で作ったコントローラーを与えます。
-              controller: controller,
-              decoration: InputDecoration(
-                // 未選択時の枠線
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                  borderSide: const BorderSide(color: Colors.amber),
+              child: CircleAvatar(
+                backgroundImage: NetworkImage(
+                  FirebaseAuth.instance.currentUser!.photoURL!,
                 ),
-                // 選択時の枠線
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                  borderSide: const BorderSide(
-                    color: Colors.amber,
-                    width: 2,
-                  ),
-                ),
-                // 中を塗りつぶす色
-                fillColor: Colors.amber[50],
-                // 中を塗りつぶすかどうか
-                filled: true,
               ),
-              onFieldSubmitted: (text) {
-                sendPost(text);
-                // 入力中の文字列を削除します。
-                controller.clear();
-              },
+            )
+          ],
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: StreamBuilder<QuerySnapshot<Post>>(
+                // stream プロパティに snapshots() を与えると、コレクションの中のドキュメントをリアルタイムで監視することができます。
+                stream: postsReference.orderBy('createdAt').snapshots(),
+                // ここで受け取っている snapshot に stream で流れてきたデータ入っています。
+                builder: (context, snapshot) {
+                  // docs には Collection に保存されたすべてのドキュメントが入ります。
+                  // 取得までには時間がかかるのではじめは null が入っています。
+                  // null の場合は空配列が代入されるようにしています。
+                  final docs = snapshot.data?.docs ?? [];
+                  return ListView.builder(
+                    itemCount: docs.length,
+                    itemBuilder: (context, index) {
+                      // data() に Post インスタンスが入っています。
+                      // これは withConverter を使ったことにより得られる恩恵です。
+                      // 何もしなければこのデータ型は Map になります。
+                      final post = docs[index].data();
+                      return PostWidget(post: post);
+                    },
+                  );
+                },
+              ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: TextFormField(
+                // 上で作ったコントローラーを与えます。
+                controller: controller,
+                decoration: InputDecoration(
+                  // 未選択時の枠線
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: const BorderSide(color: Colors.amber),
+                  ),
+                  // 選択時の枠線
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: const BorderSide(
+                      color: Colors.amber,
+                      width: 2,
+                    ),
+                  ),
+                  // 中を塗りつぶす色
+                  fillColor: Colors.amber[50],
+                  // 中を塗りつぶすかどうか
+                  filled: true,
+                ),
+                onFieldSubmitted: (text) {
+                  sendPost(text);
+                  // 入力中の文字列を削除します。
+                  controller.clear();
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -181,30 +188,44 @@ class PostWidget extends StatelessWidget {
                     ),
                   ],
                 ),
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    // 角丸にするにはこれを追加します。
-                    // 4 の数字を大きくするともっと丸くなります。
-                    borderRadius: BorderRadius.circular(4),
-                    // 色はここで変えられます
-                    // [100] この数字を小さくすると色が薄くなります。
-                    color:
-                        FirebaseAuth.instance.currentUser!.uid == post.posterId
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        // 角丸にするにはこれを追加します。
+                        // 4 の数字を大きくするともっと丸くなります。
+                        borderRadius: BorderRadius.circular(4),
+                        // 色はここで変えられます
+                        // [100] この数字を小さくすると色が薄くなります。
+                        color: FirebaseAuth.instance.currentUser!.uid ==
+                                post.posterId
                             ? Colors.amber[100]
                             : Colors.blue[100],
-                  ),
-                  child: Text(post.text),
+                      ),
+                      child: Text(post.text),
+                    ),
+
+                    /// 編集ボタン
+                    if (FirebaseAuth.instance.currentUser!.uid == post.posterId)
+                      Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Icons.edit),
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              // 削除は reference に対して delete() を呼ぶだけでよい。
+                              post.reference.delete();
+                            },
+                            icon: const Icon(Icons.delete),
+                          ),
+                        ],
+                      ),
+                  ],
                 ),
-                // List の中の場合は if 文であっても {} この波かっこはつけなくてよい
-                if (FirebaseAuth.instance.currentUser!.uid == post.posterId)
-                  IconButton(
-                    onPressed: () {
-                      // 削除は reference に対して delete() を呼ぶだけでよい。
-                      post.reference.delete();
-                    },
-                    icon: const Icon(Icons.delete),
-                  ),
               ],
             ),
           ),
