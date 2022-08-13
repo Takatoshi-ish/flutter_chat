7ba8350a15c29a227a7e31f4875ff62d63764eff
Takatoshi-ish <ishiguro.taka@gmail.com>
2022-08-13 21:49:00 +0900

    新規 Post を投稿

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,10 @@ import 'package:flutter_chat/firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_chat/post.dart';
+import './post.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-import 'chatPage.dart';
+import './chatPage.dart';
 
 Future<void> main() async {
   // main 関数でも async が使えます
