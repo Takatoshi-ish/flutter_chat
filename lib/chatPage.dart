ad76cc3e0efe96bf7f132fb7e34cbe88076eb3a8
Takatoshi-ish <ishiguro.taka@gmail.com>
2022-08-13 23:06:36 +0900

    投稿処理を関数として分離

--- lib/chatPage.dart
+++ lib/chatPage.dart
@@ -15,6 +15,33 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  Future<void> sendPost(String text) async {
+    // まずは user という変数にログイン中のユーザーデータを格納します
+    final user = FirebaseAuth.instance.currentUser!;
+
+    final posterId = user.uid; // ログイン中のユーザーのIDがとれます
+    final posterName = user.displayName!; // Googleアカウントの名前がとれます
+    final posterImageUrl = user.photoURL!; // Googleアカウントのアイコンデータがとれます
+
+    // 先ほど作った postsReference からランダムなIDのドキュメントリファレンスを作成します
+    // doc の引数を空にするとランダムなIDが採番されます
+    final newDocumentReference = postsReference.doc();
+
+    final newPost = Post(
+      text: text,
+      createdAt: Timestamp.now(), // 投稿日時は現在とします
+      posterName: posterName,
+      posterImageUrl: posterImageUrl,
+      posterId: posterId,
+      reference: newDocumentReference,
+    );
+
+    // 先ほど作った newDocumentReference のset関数を実行するとそのドキュメントにデータが保存されます。
+    // 引数として Post インスタンスを渡します。
+    // 通常は Map しか受け付けませんが、withConverter を使用したことにより Post インスタンスを受け取れるようになります。
+    newDocumentReference.set(newPost);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -68,30 +95,7 @@ class _ChatPageState extends State<ChatPage> {
           ),
           TextFormField(
             onFieldSubmitted: (text) {
-              // まずは user という変数にログイン中のユーザーデータを格納します
-              final user = FirebaseAuth.instance.currentUser!;
-
-              final posterId = user.uid; // ログイン中のユーザーのIDがとれます
-              final posterName = user.displayName!; // Googleアカウントの名前がとれます
-              final posterImageUrl = user.photoURL!; // Googleアカウントのアイコンデータがとれます
-
-              // 先ほど作った postsReference からランダムなIDのドキュメントリファレンスを作成します
-              // doc の引数を空にするとランダムなIDが採番されます
-              final newDocumentReference = postsReference.doc();
-
-              final newPost = Post(
-                text: text,
-                createdAt: Timestamp.now(), // 投稿日時は現在とします
-                posterName: posterName,
-                posterImageUrl: posterImageUrl,
-                posterId: posterId,
-                reference: newDocumentReference,
-              );
-
-              // 先ほど作った newDocumentReference のset関数を実行するとそのドキュメントにデータが保存されます。
-              // 引数として Post インスタンスを渡します。
-              // 通常は Map しか受け付けませんが、withConverter を使用したことにより Post インスタンスを受け取れるようになります。
-              newDocumentReference.set(newPost);
+              sendPost(text);
             },
           ),
         ],
