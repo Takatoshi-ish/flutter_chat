07f7574f2c3e4321d1fe93a81598b2d996218570
Takatoshi-ish <ishiguro.taka@gmail.com>
2022-10-14 11:41:22 +0900

    最新の uid をプロバイド

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -9,3 +9,7 @@ final userProvider = StreamProvider((ref) {
   final firebaseAuth = ref.read(firebaseAuthProvider);
   return firebaseAuth.userChanges();
 });
+
+final uidProvider = StreamProvider((ref) {
+  return ref.watch(userProvider.stream).map((event) => event?.uid);
+});
